9ee18870aa3253e3e2cdac346646a7717a727fb1
Michael Diaz <michaelbrian88@gmail.com>
2024-11-15 23:23:04 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,18 @@ const router = express.Router();
 router.get('/', (req, res, next) => res.render('index'));
 
 module.exports = router;
+
+const Movie = require('.../models/Movie.model');
+
+router.get('/movies', (req, res) => {
+  Movie.find()
+    .then(movies => res.render('movies', { movies }))
+    .catch(err => console.error(err));
+});
+
+router.get('/movies/:id', (req, res) => {
+    Movie.findById(req.params.id)
+      .then(movie => res.render('movie-details', { movie }))
+      .catch(err => console.error(err));
+  });
+  
